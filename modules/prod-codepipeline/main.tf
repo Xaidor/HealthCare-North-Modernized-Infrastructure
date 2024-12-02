4a7417dc2d31bfda3f80e523d80ecb6c68ad93a5
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2024-12-02 10:19:11 -0500

    updated comments

--- modules/prod-codepipeline/main.tf
+++ modules/prod-codepipeline/main.tf
@@ -1,5 +1,5 @@
  /* CodePipeline Draft as a source repo 
- THIS RESOURCE HAS NOT BEEN TESTED! 
+ THIS RESOURCE HAS NOT BEEN FULLY TESTED! CODE BUILD NEEDED
  */
 
 resource "aws_codestarconnections_connection" "HCN_github" {
